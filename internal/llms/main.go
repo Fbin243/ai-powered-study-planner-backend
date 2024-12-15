3c76f94819228f0f8fe548118456fbcec92a371e
Fbin Super Star <ntbinh243.dev@gmail.com>
2024-12-15 14:43:09 +0000

    chore: add expired status, stream message

--- internal/llms/main.go
+++ internal/llms/main.go
@@ -2,30 +2,58 @@ package main
 
 import (
 	"context"
+	"errors"
 	"fmt"
+	"io"
+	"log"
+	"os"
 
+	"github.com/joho/godotenv"
 	openai "github.com/sashabaranov/go-openai"
 )
 
 func main() {
-	client := openai.NewClient("your token")
-	resp, err := client.CreateChatCompletion(
-		context.Background(),
-		openai.ChatCompletionRequest{
-			Model: openai.GPT4oMini,
-			Messages: []openai.ChatCompletionMessage{
-				{
-					Role:    openai.ChatMessageRoleUser,
-					Content: "Hello!",
-				},
-			},
-		},
-	)
-
+	err := godotenv.Load()
 	if err != nil {
-		fmt.Printf("ChatCompletion error: %v\n", err)
-		return
+		log.Fatal("Error loading .env file")
 	}
 
-	fmt.Println(resp.Choices[0].Message.Content)
+	apiKey := os.Getenv("OPENAI_API_KEY")
+	fmt.Println("API Key:", apiKey)
+	c := openai.NewClient(apiKey)
+	ctx := context.Background()
+
+	req := openai.ChatCompletionRequest{
+		Model:     openai.GPT3Dot5Turbo,
+		MaxTokens: 100,
+		Messages: []openai.ChatCompletionMessage{
+			{
+				Role:    openai.ChatMessageRoleUser,
+				Content: "Hello world, I'm Fbin",
+			},
+		},
+		Stream: true,
+	}
+	stream, err := c.CreateChatCompletionStream(ctx, req)
+	if err != nil {
+		fmt.Printf("ChatCompletionStream error: %v\n", err)
+		return
+	}
+	defer stream.Close()
+
+	fmt.Printf("Stream response: ")
+	for {
+		response, err := stream.Recv()
+		if errors.Is(err, io.EOF) {
+			fmt.Println("\nStream finished")
+			return
+		}
+
+		if err != nil {
+			fmt.Printf("\nStream error: %v\n", err)
+			return
+		}
+
+		fmt.Printf(response.Choices[0].Delta.Content)
+	}
 }
--- internal/tasks/transport/dto/task_dto.go
+++ internal/tasks/transport/dto/task_dto.go
@@ -7,7 +7,7 @@ type TaskDto struct {
 	Name        string    `json:"name" validate:"required,min=1,max=100"`
 	Description string    `json:"description,omitempty" validate:"omitempty,max=255"`
 	Priority    string    `json:"priority,omitempty" validate:"required,oneof=low medium high"`
-	Status      string    `json:"status,omitempty" validate:"required,oneof=not_started in_progress completed"`
+	Status      string    `json:"status,omitempty" validate:"required,oneof=not_started in_progress completed expired"`
 	StartDate   time.Time `json:"start_date,omitempty" validate:"required"`
 	EndDate     time.Time `json:"end_date,omitempty" validate:"required,gtfield=StartDate"`
 }
